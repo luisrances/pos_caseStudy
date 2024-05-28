ffea72101636fc854de80e6fd0b18c12defe8ea0
luisrances <85559002+luisrances@users.noreply.github.com>
2024-05-29 00:30:24 +0800

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -9,35 +9,6 @@ function openMainPage() {
     window.location.href = "main.html";
     return true;
 }
-// function login() {
-//     // retrieve accounts
-//     var email = document.getElementById("email").value;
-//     var pass = document.getElementById("password").value;
-//     if (email == "admin" && pass == "admin") {
-//         attempt = 0;
-//         window.location.href = "main.html";
-//         document.getElementById("email").innerHTML = "";
-//         document.getElementById("password").innerHTML = "";
-//     } else {
-//         attempt++;
-//         // error
-//         document.getElementById("email").value = "";
-//         document.getElementById("password").value = "";
-//         alert("Invalid email or password");
-//         if (attempt == 3) {
-//             document.getElementById("email").disabled = true;
-//             document.getElementById("password").disabled = true;
-//             document.getElementById("btnLogin").disabled = true;
-//             document.getElementById("email").innerHTML = "";
-//             document.getElementById("password").innerHTML = "";
-//             document.getElementById("email").value = null;
-//             document.getElementById("password").value = null;
-//             alert("You have entered 3 consecutive wrong attempts. Please try again!");
-//         }
-//     }
-// }
-
-// signup
 
 let userEmails = [];
 let userPasswords = [];
@@ -58,6 +29,7 @@ function signUp(email, password) {
 
 // Login function
 function login() {
+    localStorage.setItem('userCredentials', JSON.stringify({ userEmails: [], userPasswords: [] }));
     // Retrieve the JSON string from local storage and convert it back to arrays
     let userCredentials = JSON.parse(localStorage.getItem('userCredentials'));
     userEmails = userCredentials.userEmails;
